02a3e67e285c95e245989a56cb0e6dfa4cec19ae
tomerbechler <tomerbechler1@gmail.com>
2026-03-19 09:47:07 +0200

    Added dialog

--- spotipsi-project/src/Components/MainSection/PageContent/Pages/PlaylistsPage.tsx
+++ spotipsi-project/src/Components/MainSection/PageContent/Pages/PlaylistsPage.tsx
@@ -1,16 +1,16 @@
-import { Typography, Button} from "@mui/material";
+import { Typography} from "@mui/material";
 import useStyles from "../../PageContent/FetchAndPresent/presentSongsStyles";
-
+import FormDialog from "./Components/PlaylistDialog"
 
 
 const FavoritePage:React.FC = () =>{
-
     const { classes } = useStyles();
+    const func = () => {}
     return(
         <div>
             <div className={classes.playListHeader}>
                 <Typography variant="h3" className={classes.title}>הפלייליסטים שלי</Typography>
-                <Button className={classes.addPlaylistBtn}>צור פלייליסט +</Button>
+                <FormDialog onSubmit={func}/>
             </div>
 
             <div>
